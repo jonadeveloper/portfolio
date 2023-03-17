0bceb7cc99cf196ae2b14be157caa9565966ca3c
= <=>
2023-03-17 15:19:01 -0300

    funcion de alerta cambiada a async await

--- sweetAlert.js
+++ sweetAlert.js
@@ -1,5 +1,5 @@
-function alertForm(){
-    Swal.fire({
+async function  alertForm (){
+    await Swal.fire({
         title:'Formulario enviado con exito',
         html: '<span class="colorTextAlert">Te contactare a la brevedad</span>',
         icon: 'success',
