e9c780b320b6508075d445ea05d609e4938ca08b
= <=>
2023-03-17 15:15:44 -0300

    se agrega alerta de confirmacion a formulario

---
+++ sweetAlert.js
@@ -0,0 +1,12 @@
+function alertForm(){
+    Swal.fire({
+        title:'Formulario enviado con exito',
+        html: '<span class="colorTextAlert">Te contactare a la brevedad</span>',
+        icon: 'success',
+        width: '30%',
+        padding: '1rem',
+        timer: '2500',
+        stopKeydownPropagation: false,
+        showConfirmButton: false
+    })
+}
